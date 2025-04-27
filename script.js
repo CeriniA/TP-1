73829ec3187a8a3f7dbed16ab5aa73542a1c6165
CeriniA <adriancerini@gmail.com>
2025-04-27 16:26:29 -0300

    Listo header y menu, responsive y desktop

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 let d = document;
-const $btnMenu = d.querySelector(".menu-btn");
-const $menu = d.querySelector(".menu");
-const $header = d.querySelector(".header");
-$btnMenu.addEventListener("click", (e) => {
-  $menu.classList.toggle("none");
-  $header.classList.toggle("bg-menu");
-});
+/* Menu */
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn");
+  const $menu = d.querySelector(".menu");
+  const $header = d.querySelector(".header");
+  $btnMenu.addEventListener("click", (e) => {
+    $menu.classList.toggle("is-active");
+    $header.classList.toggle("bg-menu");
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+  });
+  d.addEventListener("click", (e) => {
+    if(!e.target.matches(".menu a"))return false;
+    $menu.classList.add("none");
+    $header.classList.remove("bg-menu");
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+  });
+})(document);
