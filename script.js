68961f53e37a97e10815e24866f690cae931c5c4
CeriniA <adriancerini@gmail.com>
2025-04-27 21:06:17 -0300

    correcciones  header nav y acerca

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ let d = document;
   const $header = d.querySelector(".header");
   $btnMenu.addEventListener("click", (e) => {
     $menu.classList.toggle("is-active");
-    $header.classList.toggle("bg-menu");
+    $header.classList.toggle("bg-menu-opacity");
     $btnMenu.firstElementChild.classList.toggle("none");
     $btnMenu.lastElementChild.classList.toggle("none");
   });
   d.addEventListener("click", (e) => {
     if(!e.target.matches(".menu a"))return false;
     $menu.classList.add("none");
-    $header.classList.remove("bg-menu");
+    $header.classList.remove("bg-menu-opacity");
     $btnMenu.firstElementChild.classList.remove("none");
     $btnMenu.lastElementChild.classList.add("none");
   });
  
 })(document);
 d.addEventListener("scroll", (e) => {
-  if(window.scrollY > 100){
-    console.log("bg-menu")
-    d.querySelector(".header").classList.add("bg-menu");
+  if(window.scrollY > 250){
+
+    d.querySelector(".header").classList.add("bg-menu-scroll");
   }else{
-    d.querySelector(".header").classList.remove("bg-menu");
+    d.querySelector(".header").classList.remove("bg-menu-scroll");
   }
 })
